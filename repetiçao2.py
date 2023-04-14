f794d40bab2bbd2390060059558cea22c68f9690
unknown <duarte.emanuellys2@gmail.com>
2023-04-14 11:19:28 -0300

    repetiçao 2 finalizada

---
+++ repetiçao2.py
@@ -0,0 +1,8 @@
+print("insira um nome de usuario")
+usuario = (input)
+print("insira uma senha")
+senha = input()
+while usuario == senha:
+    print("insira uma senha nova do usuario")
+    senha = input()
+
